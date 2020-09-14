4b3039193e0c8ae0265a351fa2aa23e9aa7139d2
Manish Wagle <waglemanish28@gmail.com>
2020-09-14 20:54:32 +0545

    git master info added

--- application/milestone0/src/HomePage.js
+++ application/milestone0/src/HomePage.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import './HomePage.css';
-import Developer from './Developer';
+import React from "react";
+import "./HomePage.css";
+import Developer from "./Developer";
 
 function HomePage() {
-    return (
-        <div className="homePage" style={{backgroundColor:"#000000"}}>
-            <div className="developers"style={{display:"flex", width:"100vw",flexWrap:"wrap",justifyContent:"space-between",alignItems:"center"}}>
-                <Developer name='Himanshu Garg' role='Team Lead' bio='I am a Computer Science Junior at SFSU.
+  return (
+    <div className="homePage" style={{ backgroundColor: "#000000" }}>
+      <div
+        className="developers"
+        style={{
+          display: "flex",
+          width: "100vw",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Developer
+          name="Himanshu Garg"
+          role="Team Lead"
+          bio="I am a Computer Science Junior at SFSU.
                 I am currently working as a Teaching Assistant for CSC 220, CSC 210 and Instructor for SCI 211.
-                I love to create web and android based applications.' image={require('./images/himanshu.jpg')}/>
-                <Developer name='Second Member' role='My Role' bio='My Bio.' image={require('./images/no-pic.jpg')}/>
-                <Developer name='Third Member' role='My Role' bio='My Bio' image={require('./images/no-pic.jpg')}/>
-                <Developer name='Fourth Member' role='My Role' bio='My Bio' image={require('./images/no-pic.jpg')}/>
-                <Developer name='Fifth Member' role='My Role' bio='My Bio' image={require('./images/no-pic.jpg')}/>
-                <Developer name='Sixth Member' role='My Role' bio='My Bio' image={require('./images/no-pic.jpg')}/>
-            </div>
-
-            
-        </div>
-    )
+                I love to create web and android based applications."
+          image={require("./images/himanshu.jpg")}
+        />
+        <Developer
+          name="Second Member"
+          role="My Role"
+          bio="My Bio."
+          image={require("./images/no-pic.jpg")}
+        />
+        <Developer
+          name="Suman Basaula"
+          role="Github Master"
+          bio="Hi, I am a computer science junior at SF state. I am very interested in creating websites and web applications. I love playing sports and learning new languages."
+          image={require("./images/suman.jpg")}
+        />
+        <Developer
+          name="Fourth Member"
+          role="My Role"
+          bio="My Bio"
+          image={require("./images/no-pic.jpg")}
+        />
+        <Developer
+          name="Fifth Member"
+          role="My Role"
+          bio="My Bio"
+          image={require("./images/no-pic.jpg")}
+        />
+        <Developer
+          name="Sixth Member"
+          role="My Role"
+          bio="My Bio"
+          image={require("./images/no-pic.jpg")}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
