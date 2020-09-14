f7f8d6dfd19944a11b840c055a3714f1898e583d
Himanshu Garg <hgarg701@gmail.com>
2020-09-13 23:35:29 -0700

    Updated server.js

--- application/milestone0/server.js
+++ application/milestone0/server.js
@@ -1,18 +1,18 @@
+
+
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
+//app.use(express.static(path.join(__dirname, "build")));
 
-
-​
 const app = express();
-​
+
+//app.get("/*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 app.use(express.static(path.join(__dirname, "build")));
-app.use(bodyParser.json());
-​
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log("started on port:", port);
-});
+
+app.get("/*", function(req, res){
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+})
+ console.log("Hello");
+app.listen(5000);
