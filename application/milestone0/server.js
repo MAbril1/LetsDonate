2c1ee16eff611e01c75b305d1ac36b8374022179
Himanshu Garg <hgarg701@gmail.com>
2020-09-16 12:53:39 -0700

    Removed unnecessary comments

--- application/milestone0/server.js
+++ application/milestone0/server.js
@@ -2,17 +2,13 @@
 
 const express = require("express");
 const path = require("path");
-//app.use(express.static(path.join(__dirname, "build")));
 
 const app = express();
 
-//app.get("/*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", function(req, res){
     res.sendFile(path.join(__dirname, "build", "index.html"));
 })
- console.log("Hello");
+
 app.listen(5000);
