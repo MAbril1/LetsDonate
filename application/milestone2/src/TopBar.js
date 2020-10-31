ffd01c4baae6cb3b9f1165a1ec8bbe17e03b7cf9
Himanshu Garg <hgarg701@gmail.com>
2020-10-31 13:12:25 -0700

    Form for Creating a post

--- application/milestone2/src/TopBar.js
+++ application/milestone2/src/TopBar.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TopBar.css';
 import charity from './images/charity.png';
 import SearchIcon from '@material-ui/icons/Search';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import { Button } from "@material-ui/core"
+import { Button } from "@material-ui/core";
+import Modal from 'react-modal';
+
 
 function TopBar() {
+    const [formIsOpen, setformISOpen] = useState(false)
     return (
         <div className="topBar">
             <div style={{display:"flex", alignItems:"center"}}>
@@ -24,10 +27,18 @@ function TopBar() {
             </div>
 
             <div className="topRight">
-                <Button variant='outlined'>Become a Donor</Button>
+                <Button variant='outlined' onClick={() => setformISOpen(true)}>Become a Donor</Button>
+                <Modal isOpen={formIsOpen}>
+                    <h2>Create Post</h2>
+                    <p>Make a post here</p>
+                    <div className='createPostButton'>
+                        <Button variant='outlined' onClick={() => setformISOpen(false)}>Create Post</Button>
+                    </div>
+                </Modal>
                 <ExpandMoreIcon />
                 <AccountCircleIcon />
             </div>
+
         </div>
 
     )
