5b21e64f84709612315ddd7ce4c05ad0de2da276
Himanshu Garg <hgarg701@gmail.com>
2020-10-31 14:46:10 -0700

    Completed the form

--- application/milestone2/src/TopBar.js
+++ application/milestone2/src/TopBar.js
@@ -5,11 +5,14 @@ import SearchIcon from '@material-ui/icons/Search';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { Button } from "@material-ui/core";
-import Modal from 'react-modal';
+
+import {Form, Label, Input, FormGroup, CustomInput, Modal, ModalBody} from 'reactstrap';
 
 
 function TopBar() {
-    const [formIsOpen, setformISOpen] = useState(false)
+    const [formIsOpen, setformISOpen] = useState(false);
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
     return (
         <div className="topBar">
             <div style={{display:"flex", alignItems:"center"}}>
@@ -28,13 +31,37 @@ function TopBar() {
 
             <div className="topRight">
                 <Button variant='outlined' onClick={() => setformISOpen(true)}>Become a Donor</Button>
-                <Modal isOpen={formIsOpen}>
-                    <h2>Create Post</h2>
-                    <p>Make a post here</p>
-                    <div className='createPostButton'>
-                        <Button variant='outlined' onClick={() => setformISOpen(false)}>Create Post</Button>
-                    </div>
-                </Modal>
+                <div>
+                    <Modal isOpen={formIsOpen} onRequestClose={() => setformISOpen(false)}>
+                        <ModalBody>
+                            <Form>
+                                <FormGroup>
+                                    <Label>Name of Product</Label>
+                                    <Input value={name}
+                                        onChange={ev => {
+                                        setName(ev.target.value);
+                                        }}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label>Description of Product</Label>
+                                    <Input value={description}
+                                        onChange={ev => {
+                                        setDescription(ev.target.value);
+                                        }}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label>Upload Product Image</Label>
+                                    <CustomInput type="file" id="productImage" accept="image/jpg,image/jpeg,image/png"/>
+                                </FormGroup>
+                            </Form>
+                        </ModalBody>
+                        <div className='createPostButton'>
+                            <Button variant='outlined' onClick={() => setformISOpen(false)}>Create Post</Button>
+                        </div>
+                    </Modal>
+                </div>
                 <ExpandMoreIcon />
                 <AccountCircleIcon />
             </div>
