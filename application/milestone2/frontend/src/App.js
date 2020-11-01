71a348696fdc170c33eefc7256a75532a8b48bc0
Himanshu Garg <hgarg701@gmail.com>
2020-10-31 23:10:29 -0700

    Refactoring

---
+++ application/milestone2/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ProductDonationHome from './ProductDonationHome.js';
+import TopBar from './TopBar';
+
+function App() {
+  return (
+    <div className="app">
+      <TopBar />
+      <ProductDonationHome />
+    </div>
+  );
+}
+
+export default App;
