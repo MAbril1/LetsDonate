ac8cc06aa9c69419dc952d9c565f1e56faf0e684
Himanshu Garg <hgarg701@gmail.com>
2020-11-01 20:10:58 -0800

    Some changes

--- application/milestone2/frontend/src/Card.js
+++ application/milestone2/frontend/src/Card.js
@@ -1,26 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import "./Card.css";
-import charity from '../images/charity.png';
+import charity from './images/charity.png';
  
 
 function Card( {name, description, productImage} ) {
 
-
-    const [image, setImage] = useState(require('../images/charity.png'));
+    /*
+    const [image, setImage] = useState(require('./images/charity.png'));
     
     console.log(productImage);
     useEffect(() => {
         if(productImage!='' && productImage!=null){
-            setImage( require(`../images/${productImage}`));
+            setImage( require(`./images/${productImage}`));
             console.log(productImage);
         }
         console.log(image);
     }, [productImage])
+    */
     
     return (
         <div className="card">
-            <img style={{width:200, height:200}} className="cardImage" 
-            src={require('../images/charity.png')} 
+            <img  className="cardImage" 
+            src={charity} 
             alt=""/>
 
     <h2>{name}</h2>
