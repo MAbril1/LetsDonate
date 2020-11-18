b456b5738eff9a84acd4c70a5b5c3b069eea1f6b
Marlon Bustamante <marlon.s.bustamante@gmail.com>
2020-11-18 13:51:07 -0800

    Used onError() to check if image is loadable

--- application/milestone3/frontend/src/components/Card.js
+++ application/milestone3/frontend/src/components/Card.js
@@ -2,26 +2,12 @@ import React, {useState, useEffect} from 'react';
 import "./Card.css"; 
 
 function Card( {name, description, productImage} ) {
-    
+
     let image;
     
     if(productImage !== '' && productImage !== null) // checks if product has an image from the database
     {
-        // checks if the image exists on the server, if not, use the default image
-        fetch("../images/" + productImage)
-            .then(res => {
-            if(res.status == 404)
-            {
-                image = "../images/charity.png";
-            }
-            else
-            {
-                image = "../images/" + productImage;
-            }
-        })
-        .catch(err=>{image = "../images/charity.png";})
-
-        image = '../images/' + productImage;
+        image = "../images/" + productImage;
     }  
     else
     {
@@ -31,7 +17,10 @@ function Card( {name, description, productImage} ) {
     return (
         <div className="card">
             <img  className="cardImage" 
-            src={image} 
+            src={image}
+            onError={(e) => {
+                e.target.src = '../images/charity.png' // fallback image
+             }} 
             alt=""/>
 
     <h2>{name}</h2>
