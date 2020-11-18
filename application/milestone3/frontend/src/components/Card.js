ac08dbbfda5f1d659b3be2e10117f942f9d7991a
Marlon Bustamante <marlon.s.bustamante@gmail.com>
2020-11-18 13:29:43 -0800

    Update Card.js

--- application/milestone3/frontend/src/components/Card.js
+++ application/milestone3/frontend/src/components/Card.js
@@ -1,14 +1,37 @@
 import React, {useState, useEffect} from 'react';
-import "./Card.css";
-import charity from '../images/charity.png';
- 
+import "./Card.css"; 
 
 function Card( {name, description, productImage} ) {
+    
+    let image;
+    
+    if(productImage !== '' && productImage !== null) // checks if product has an image from the database
+    {
+        // checks if the image exists on the server, if not, use the default image
+        fetch("../images/" + productImage)
+            .then(res => {
+            if(res.status == 404)
+            {
+                image = "../images/charity.png";
+            }
+            else
+            {
+                image = "../images/" + productImage;
+            }
+        })
+        .catch(err=>{image = "../images/charity.png";})
+
+        image = '../images/' + productImage;
+    }  
+    else
+    {
+        image = "../images/charity.png"; // default image
+    }
     
     return (
         <div className="card">
             <img  className="cardImage" 
-            src={charity} 
+            src={image} 
             alt=""/>
 
     <h2>{name}</h2>
