3b594c780cd1edcc305af67b325a0b6529f8c602
kunyuruan <kruan1@mail.sfsu.edu>
2020-12-07 23:11:04 -0800

    Update my branch

--- application/milestone3/frontend/src/components/register.js
+++ application/milestone3/frontend/src/components/register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './register.css';
 
 // contains function needed to send form information to database
 import formData from './backend/registerBackend.js';
@@ -8,32 +9,36 @@ function register() {
     // very basic user registration form
     // gets information needed to get sent to the database
     // buttons calls function from registerBackend.js
-    
+
     return (
-        <form id="registerForm" method="post">
-            <h1>Signup to Let's Donate</h1>
-            <label>
-                Name:
-                <input type="text" name="name" placeholder="John Smith"/>
-            </label>
-            <label>
-                Email:
-                <input type="email" name="email" placeholder="JohnSmith@email.com"/>
-            </label>
-            <label>
-                Zip Code:
-                <input type="number" name="zipcode" placeholder="12345"/>
-            </label>
-            <label>
-                Password:
-                <input type="password" name="password" placeholder="password"/>
-            </label>
-            <label>
-                Confirm Password:
-                <input type="password" name="password2" placeholder="password"/>
-            </label>
-            <button type="button" onClick={() => {formData()}}>Signup</button>
-        </form>
+        <div>
+            <div className='form-header'>
+                <h1>Sign Up to Let's Donate</h1>
+                <hr /><br />
+            </div>
+            <div className='form-body'>
+                <form id="registerForm" method="post">
+                    <label for="name"><b>Name</b></label><br />
+                    <input type="text" name="name" placeholder="John Smith" /><br />
+
+                    <label for="email"><b>Email</b></label><br />
+                    <input type="email" name="email" placeholder="Enter Email" /><br />
+
+                    <label for="password"><b>Password</b></label><br />
+                    <input type="password" name="password" placeholder="Enter Password" /><br />
+
+                    <label for="password2"><b>Confirm Password</b></label><br />
+                    <input type="password" name="password2" placeholder="Comfirm Password" /><br />
+
+                    <label for="zipcode"><b>Zip Code</b></label><br />
+                    <input type="number" name="zipcode" placeholder="Enter Zip Code" /><br />
+
+                    <div className="form-button">
+                        <button type="button" onClick={() => { formData() }}>Sign Up</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
