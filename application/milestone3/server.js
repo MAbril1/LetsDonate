7bd369bd8aa50b4a955e9849fe5b28041a8e8226
Marlon Bustamante <marlon.s.bustamante@gmail.com>
2020-12-16 21:23:37 -0800

    Update server.js

--- application/milestone3/server.js
+++ application/milestone3/server.js
@@ -282,7 +282,7 @@ app.post('/api/updateEndorsement', function (req, res) {
 app.use(express.static(path.join(__dirname, "frontend/build")));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
 })
 
 app.listen(5000);
